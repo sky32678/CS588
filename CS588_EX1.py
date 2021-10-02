1c4e4f9d17a622c82112fa2d5106aba8298016ac
Woojin Ahn <wahn3@illinois.edu>
2021-10-01 21:03:21 -0500

    minor change

--- CS588_EX1.py
+++ CS588_EX1.py
@@ -1,43 +1,37 @@
 import rospy
-from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist
 from pacmod_msgs.msg import PacmodCmd
 
-def stop_light():
-    pacmod_msg = PacmodCmd()
+def stop_light(pacmod_msg):
     pacmod_msg.TURN_NONE = 1
     pub.publish(pacmod_msgs)
-    return
 
-def right_light():
-    pacmod_msg = PacmodCmd()
+def right_light(pacmod_msg):
     pacmod_msg.TURN_RIGHT = 0
     pub.publish(pacmod_msgs)
-    return
 
-def left_light():
-    pacmod_msg = PacmodCmd()
+def left_light(pacmod_msg):
     pacmod_msg.TURN_LEFT = 2
     pub.publish(pacmod_msgs)
-    return
 
 if __name__ == "__main__":
-    rospy.init_node('check_odometry')
+    rospy.init_node('Polaris GEM 2 Ex')
     pub = rospy.Publisher("/pacmod/as_rx/turn_cmd", PacmodCmd, queue_size = 10)
     while not rospy.is_shutdown():
         while not pub.get_num_connections() == 1:
             pass
 
-        left_light()
-        rospy.sleep(3)
-        stop_light()
+        pacmod_msg = PacmodCmd()
 
-        right_light()
+        left_light(pacmod_msg)
         rospy.sleep(3)
-        stop_light()
+        stop_light(pacmod_msg)
 
-        left_light()
+        right_light(pacmod_msg)
         rospy.sleep(3)
-        stop_light()
+        stop_light(pacmod_msg)
+
+        left_light(pacmod_msg)
+        rospy.sleep(3)
+        stop_light(pacmod_msg)
 
         rospy.sleep(3)
