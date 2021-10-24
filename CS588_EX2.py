8c513267a777253dc7ab7dfad3289431bffe902d
Woojin Ahn <wahn3@illinois.edu>
2021-10-24 18:29:41 -0500

    cv_bridge added

--- CS588_EX2.py
+++ CS588_EX2.py
@@ -1,6 +1,35 @@
 import rospy
 from pacmod_msgs.msg import PacmodCmd
+import cv2
 import torch
+from cv_bridge import CvBridge, CvBridgeError
+##FOr Webcam
+class image_converter:
+    def __init__(self):
+        # self.image_pub = rospy.Publisher("image_topic_2",Image)
+
+        self.bridge = CvBridge()
+        # self.image_sub = rospy.Subscriber("image_topic",Image,self.callback)
+        self.image_sub = rospy.Subscriber("/mako_1/mako_1/image_raw", PacmodCmd, self.callback)
+
+    def callback(self,data):
+        global cv_image
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        (rows,cols,channels) = cv_image.shape
+        if cols > 60 and rows > 60 :
+            cv2.circle(cv_image, (50,50), 10, 255)
+            cv2.imshow("Image window", cv_image)
+            cv2.waitKey(3)
+
+        # try:
+        #     self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+        # except CvBridgeError as e:
+        #     print(e)
+
 
 
 def brake(pub):
@@ -25,10 +54,10 @@ def callback(msg):
 
 
 def main():
-    global cam_image
+    global cv_image
     rospy.init_node('Polaris GEM 2 Ex')
     pub = rospy.Publisher("/pacmod/as_rx/turn_cmd", PacmodCmd, queue_size = 10)
-    sub = rospy.Subscriber("/mako_1/mako_1/image_raw", PacmodCmd, callback)
+    # sub = rospy.Subscriber("/mako_1/mako_1/image_raw", PacmodCmd, callback)
     #YOLO Network Here
     model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
     count = 0
@@ -37,7 +66,7 @@ def main():
             pass
 
         ##What we need for image detection
-        results = model(cam_image) ## frame == image
+        results = model(cv_image) ## frame == image
         temp = results.xyxy[0][:,5].tolist()
         #image from the camera
         if 0.0 in temp:
